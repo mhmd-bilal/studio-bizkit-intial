bd8f3b49e688d34eb31e15d90e1d8230f68c1d72
bilal <bilal@flooid.in>
2024-12-02 11:05:01 +0530

    Updated config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: 'localhost',
+          port: '3000',
+          pathname: '/images/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'https://lively-treacle-4903c1.netlify.app',
+          pathname: '/images/**',
+        },
+        {
+            protocol: 'https',
+            hostname: 'https://studiobizkit.com',
+            pathname: '/images/**',
+          },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
