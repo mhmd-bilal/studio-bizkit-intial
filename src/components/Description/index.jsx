49623903387b595cc1263cb6f97537158b6c3a6e
bilal <bilal@flooid.in>
2024-12-02 10:06:52 +0530

    Initial commit

---
+++ src/components/Description/index.jsx
@@ -0,0 +1,42 @@
+import styles from "./style.module.scss";
+import { useInView, motion } from "framer-motion";
+import { useRef } from "react";
+import { slideUp, opacity } from "./animation";
+import Rounded from "../../common/RoundedButton";
+export default function index() {
+  const phrase =
+    "Helping brands to stand out in the digital era. Together we will set the new status quo. No nonsense, always on the cutting edge.";
+  const description = useRef(null);
+  const isInView = useInView(description);
+  return (
+    <div ref={description} className={styles.description}>
+      <div className={styles.body}>
+        <p>
+          {phrase.split(" ").map((word, index) => {
+            return (
+              <span key={index} className={styles.mask}>
+                <motion.span
+                  variants={slideUp}
+                  custom={index}
+                  animate={isInView ? "open" : "closed"}
+                  key={index}
+                >
+                  {word}
+                </motion.span>
+              </span>
+            );
+          })}
+        </p>
+        <motion.p variants={opacity} animate={isInView ? "open" : "closed"}>
+          We specialize in creating, branding, and crafting web experiences that
+          are baked to perfection—fresh, crisp, and irresistibly engaging.
+        </motion.p>
+        <div data-scroll data-scroll-speed={0.1}>
+          <Rounded backgroundColor={"#EE3224"} className={styles.button}>
+            <p>About us</p>
+          </Rounded>
+        </div>
+      </div>
+    </div>
+  );
+}
